6da23865fa0aa33088c5bf22f32b1959a33cf7ee
Eric Schulte <schulte.eric@gmail.com>
2014-01-02 14:01:02 -0700

    add commentary

--- src/org-ehtml.el
+++ src/org-ehtml.el
@@ -23,6 +23,17 @@
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
+;;; Commentary:
+
+;; View and edit Org-mode files through a web server.  Org-ehtml
+;; defines a new export backend for Org-mode (ox-ehtml) which extends
+;; the default HTML exporter with Javascript to allow for editing of
+;; the web page.  Org-ehtml serves Org-mode files exported with
+;; ox-ehtml using the Emacs Web Server allowing for Org-mode files to
+;; be edited interactively through the web browser and for the edits
+;; to be applied to the local Org-mode files on disk and optionally
+;; committed to a backing version control repository.
+
 ;;; Code:
 (let ((default-directory (file-name-directory
                           (or load-file-name (buffer-file-name)))))
