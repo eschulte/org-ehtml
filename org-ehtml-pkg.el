9f19a9662ebd28a838dddd1817f89f9194f67eda
Eric Schulte <schulte.eric@gmail.com>
2014-01-09 23:49:31 -0700

    instructions for adding authorization

--- org-ehtml-pkg.el
+++ org-ehtml-pkg.el
@@ -1,3 +1,3 @@
 (define-package "org-ehtml" (format-time-string "%Y%m%d.%H%M")
   "Export Org-mode files as editable web pages"
-  '((web-server "20140103.15") (emacs "24.3")))
+  '((web-server "20140109.2200") (emacs "24.3")))
